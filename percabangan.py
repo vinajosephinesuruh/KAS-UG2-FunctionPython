58d7a295877ecc7362eadaa7784e5e60adf955af
vinajosephinesuruh <vina.josephine@ti.ukdw.ac.id>
2023-07-27 12:57:56 +0700

    Create percabangan.py

---
+++ percabangan.py
@@ -0,0 +1,67 @@
+
+
+
+
+print("++++++PROGRAM KONVERSI SUHU++++++")
+
+def konversi_suhu():
+    celcius = 88
+    R = (4/5)*celcius
+    hasil = R
+    print("reamur ke celcius :",hasil)
+
+konversi_suhu()
+
+def konversi_suhu():
+    celcius = 88
+    R = (4/5)*celcius
+    hasil = R
+    print("reamur ke celcius :",hasil)
+
+konversi_suhu()
+
+
+def celsius_to_reamur(celsius):
+    reamur = 4/5 * celsius
+    return reamur
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = 9/5 * celsius + 32
+    return fahrenheit
+
+def celsius_to_kelvin(celsius):
+    kelvin = celsius + 273
+    return kelvin
+
+# Input suhu dalam Celsius
+celsius = 88
+
+# Konversi suhu
+reamur = celsius_to_reamur(celsius)
+fahrenheit = celsius_to_fahrenheit(celsius)
+kelvin = celsius_to_kelvin(celsius)
+
+# Hasil konversi
+print(f"{celsius} derajat Celsius sama dengan {reamur:.2f} derajat Reamur")
+print(f"{celsius} derajat Celsius sama dengan {fahrenheit:.2f} derajat Fahrenheit")
+print(f"{celsius} derajat Celsius sama dengan {kelvin:.2f} Kelvin")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
